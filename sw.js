0cb80a2eae3ea266def247e932d4252f63df6abf
louis06151989 <l.harman215@gmail.com>
2026-08-28 23:06:29 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fitbank-v4-0-20260828';
+const CACHE='fitbank-v4-1-20260828';
 const ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
