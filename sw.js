0f31b7b18cf1eac03de84ede09b1372b2c05071c
louis06151989 <l.harman215@gmail.com>
2026-08-28 22:46:56 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE='fitbank-v3-8-20260828';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='fitbank-v3-9-20260828';
+const ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener('fetch',e=>{
+  const req=e.request;
+  if(req.mode==='navigate'){
+    e.respondWith(fetch(req,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(fetch(req).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(req,c));return r}).catch(()=>caches.match(req)));
+});
