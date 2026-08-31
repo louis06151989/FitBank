02a6e78b7c0e25758a065ce8822d8e0d324f049e
louis06151989 <l.harman215@gmail.com>
2026-08-31 18:23:28 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,26 @@
-const CACHE='fitbank-v5-3-20260831';
-const ASSETS=['./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  const req=e.request;
+const CACHE='fitbank-v6-0-clean-20260831';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>Promise.allSettled(CORE.map(url=>cache.add(url)))));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET')return;
   if(req.mode==='navigate'){
-    e.respondWith(fetch(req,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
+    event.respondWith(fetch(req,{cache:'no-store'}).then(res=>{
+      const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res;
+    }).catch(()=>caches.match('./index.html')));
     return;
   }
-  e.respondWith(fetch(req).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(req,c));return r}).catch(()=>caches.match(req)));
+  const url=new URL(req.url);
+  if(url.origin===self.location.origin){
+    event.respondWith(caches.match(req).then(cached=>{
+      const fresh=fetch(req).then(res=>{if(res&&res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy));}return res;}).catch(()=>cached);
+      return cached||fresh;
+    }));
+  }
 });
